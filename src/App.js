16b16c3576ea01181b6ee0ec3f5961479605ed27
Gabriel Karczewski <nickelghost@outlook.com>
2020-10-18 20:33:39 +0200

    Add react-admin with carousel items listing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Admin,
+  Resource,
+  TextField,
+  DateField,
+  Datagrid,
+  UrlField,
+  List,
+  ImageField,
+} from "react-admin";
+import simpleRestProvider from "ra-data-simple-rest";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const dataProvider = simpleRestProvider("http://localhost:4000/admin");
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="carousel-items"
+      list={(props) => (
+        <List {...props}>
+          <Datagrid>
+            <TextField label="ID" source="id" />
+            <TextField source="title" />
+            <TextField source="description" />
+            <ImageField label="Picture" source="pictureUrl" />
+            <UrlField label="Destination URL" source="destinationUrl" />
+            <DateField showTime source="createdAt" />
+            <DateField showTime source="updatedAt" />
+          </Datagrid>
+        </List>
+      )}
+    />
+  </Admin>
+);
 
 export default App;
